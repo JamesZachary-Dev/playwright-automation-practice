e88e520fb3e5a30fa60c4de1d1d1e6e0eafa3aae
JamesZachary-Dev <Bailyjameszachary@outlook.com>
2026-03-16 00:25:10 +1100

    update project files

---
+++ pages/loginpage.ts
@@ -0,0 +1,14 @@
+import { Page } from '@playwright/test';
+
+export class LoginPage {
+    readonly page: Page;
+
+    constructor(page: Page) {
+        this.page = page;
+    }
+
+    async goto() {
+  await this.page.goto('https://www.saucedemo.com/');
+  
+}
+}
--- tests/saucedemo.spec.ts
+++ tests/saucedemo.spec.ts
@@ -1,23 +1,17 @@
-import {test, expect } from '@playwright/test'
+import { test, expect } from '@playwright/test';
+import { login } from '../helpers/login';
 
 test('SauceDemo successful login', async ({ page }) => {
-
   await page.goto('https://www.saucedemo.com/');
-
-  await page.getByPlaceholder('Username').fill('standard_user');
-  await page.getByPlaceholder('Password').fill('secret_sauce');
-
-  await page.getByRole('button', { name: 'Login' }).click();
+  await login(page);
 
   await expect(page).toHaveURL(/inventory/);
-
   await expect(page.locator('.title')).toHaveText('Products');
 });
 
 test('SauceDemo login fails with invalid password', async ({ page }) => {
-
   await page.goto('https://www.saucedemo.com/');
-
+  
   await page.getByPlaceholder('Username').fill('standard_user');
   await page.getByPlaceholder('Password').fill('wrong_password');
 
@@ -26,4 +20,13 @@ test('SauceDemo login fails with invalid password', async ({ page }) => {
   await expect(page.locator('[data-test="error"]')).toHaveText(
     'Epic sadface: Username and password do not match any user in this service'
   );
+});
+
+test('User can add Backpack to cart', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await login(page);
+
+  await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+
+  await expect(page.locator('.shopping_cart_badge')).toHaveText('1');
 });
